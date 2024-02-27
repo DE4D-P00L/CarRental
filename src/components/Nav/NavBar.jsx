11170a450902ce33ca44759660bed11569024d6c
Prashant <jatinverma200025@gmail.com>
2024-02-27 13:19:24 +0530

    fixed sticky nav

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 const NavBar = ({ dark, setDark }) => {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <nav className="sticky h-[60px] w-full flex items-center justify-center border-b border-content z-[99] bg-base-100 overflow-hidden">
+    <nav className="sticky h-[60px] w-full flex items-center justify-center border-b border-content z-[99] bg-base-100 overflow-hidden top-0">
       <div className="flex items-center justify-between px-2 max-w-7xl w-full">
         <Logo />
         <DesktopMenu dark={dark} setDark={setDark} />
